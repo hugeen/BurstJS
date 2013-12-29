8057dd336af5b9f11a7381dd48f9ae7d0dd60ed6
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2013-12-29 01:22:05 +0100

    Clean Blank Example

--- examples/blank/game.js
+++ examples/blank/game.js
@@ -3,29 +3,8 @@ define(function(require) {
     var Player = require("models/player");
     var entities = require("collections/entities");
 
-    Player.create().tag("hhh").tag("aaa").tag("zzz");
-    Player.create().tag("aaa");
-    Player.create().tag("hhh");
-    Player.create().tag("hhh");
-    Player.create().tag("hhh");
-    Player.create().tag("zzz");
-    Player.create().tag("hhh");
-    Player.create().tag("aaa");
-    Player.create().tag("hhh");
-    Player.create().tag("hhh");
-    Player.create().tag("salut");
-
-    Player.on(["test", "test"], function() {
-        console.log("test");
-    });
-
-    Player.on(["hello", "test"], function() {
-        console.log("hello");
-    });
-
-    Player.emit(["test", "hello", "test"]);
     game.on("start", function() {
-        // scenesController.emit("render", "loading");
+        
     });
 
     return game;
