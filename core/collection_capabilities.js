a94ed9cca1e360b0e2325deb1efe3ba186db0c14
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-01-11 17:31:40 +0100

    Decouple tags from collections

--- core/collection_capabilities.js
+++ core/collection_capabilities.js
@@ -1,34 +1,28 @@
 define(function(require) {
 
-    var finderCapabilities = require("burst/core/finder");
+    var finderCapabilities = require("burst/core/finder_capabilities");
+    var tagCapabilities = require("burst/core/tag_capabilities");
     var slice = Array.prototype.slice;
 
     return function(collection) {
 
-        var flattened = Object.create(Array.prototype);
-        finderCapabilities(flattened);
+        collection.all = Object.create(Array.prototype);
 
-        var tags = {};
-        var tagNames = [];
-
-        Object.defineProperty(collection, "all", {
-            value: flattened,
-            writable: false,
-            enumerable: true
-        });
+        finderCapabilities(collection.all);
+        tagCapabilities(collection);
 
         collection.find = function() {
-            return flattened.find.apply(flattened, slice.call(arguments));
+            return collection.all.find.apply(collection.all, slice.call(arguments));
         };
 
         collection.where = function() {
-            return flattened.where.apply(flattened, slice.call(arguments));
+            return collection.all.where.apply(collection.all, slice.call(arguments));
         };
 
         collection.add = function(item) {
             var items = Array.isArray(item) ? item : [item];
             items.forEach(function(item) {
-                flattened.push(item);
+                collection.all.push(item);
             });
 
             return flattened;
@@ -38,76 +32,14 @@ define(function(require) {
             var items = Array.isArray(item) ? item : [item];
             collection.untagAll(item);
             items.forEach(function(item) {
-                var index = flattened.indexOf(item);
+                var index = collection.all.indexOf(item);
                 if (index !== -1) {
-                    flattened.splice(index, 1);
+                    collection.all.splice(index, 1);
                 }
             });
 
-            return flattened;
+            return collection.all;
         };
-
-        collection.tag = function(tagName, item) {
-            var tag = tags[tagName] || createTag(tagName);
-            tag.push(item);
-
-            return collection;
-        };
-
-        collection.untag = function(tagName, item) {
-            var tag = tags[tagName] || false;
-            if (!tag) {
-                return collection;
-            }
-            var index = tag.indexOf(item);
-            if (index !== -1) {
-                tag.splice(index, 1);
-            }
-
-            return collection;
-        };
-
-        collection.untagAll = function(item) {
-            tagNames.forEach(function(tagName) {
-                collection.untag(tagName, item);
-            });
-
-            return collection;
-        };
-
-        function createTag(tagName) {
-            tagNames.push(tagName);
-            tags[tagName] = Object.create(Array.prototype);
-            finderCapabilities(tags[tagName]);
-            Object.defineProperty(collection, tagName, {
-                get: function() {
-                    return tagCascade(tags[tagName]);
-                },
-                configurable: true
-            });
-
-            return tags[tagName];
-        }
-
-        function tagCascade(parentTag) {
-            tagNames.forEach(function(tagName) {
-                Object.defineProperty(parentTag, tagName, {
-                    get: function() {
-                        var filtered = [];
-                        tags[tagName].forEach(function(item) {
-                            if (parentTag.indexOf(item) !== -1) {
-                                filtered.push(item);
-                            }
-                        });
-                        finderCapabilities(filtered);
-                        return tagCascade(filtered);
-                    },
-                    configurable: true
-                });
-            });
-
-            return parentTag;
-        }
 
         return collection;
     };
---
+++ core/tag_capabilities.js
@@ -0,0 +1,75 @@
+define(function(require) {
+
+    var finderCapabilities = require("burst/core/finder_capabilities");
+
+    return function(object) {
+
+        var tags = {};
+        var tagNames = [];
+
+        object.tag = function(tagName, item) {
+            var tag = tags[tagName] || createTag(tagName);
+            tag.push(item);
+
+            return object;
+        };
+
+        object.untag = function(tagName, item) {
+            var tag = tags[tagName] || false;
+            if (!tag) {
+                return object;
+            }
+            var index = tag.indexOf(item);
+            if (index !== -1) {
+                tag.splice(index, 1);
+            }
+
+            return object;
+        };
+
+        object.untagAll = function(item) {
+            tagNames.forEach(function(tagName) {
+                object.untag(tagName, item);
+            });
+
+            return object;
+        };
+
+        function createTag(tagName) {
+            tagNames.push(tagName);
+            tags[tagName] = Object.create(Array.prototype);
+            finderCapabilities(tags[tagName]);
+            Object.defineProperty(object, tagName, {
+                get: function() {
+                    return tagCascade(tags[tagName]);
+                },
+                configurable: true
+            });
+
+            return tags[tagName];
+        }
+
+        function tagCascade(parentTag) {
+            tagNames.forEach(function(tagName) {
+                Object.defineProperty(parentTag, tagName, {
+                    get: function() {
+                        var filtered = Object.create(Array.prototype);
+                        tags[tagName].forEach(function(item) {
+                            if (parentTag.indexOf(item) !== -1) {
+                                filtered.push(item);
+                            }
+                        });
+                        finderCapabilities(filtered);
+                        return tagCascade(filtered);
+                    },
+                    configurable: true
+                });
+            });
+
+            return parentTag;
+        }
+
+        return object;
+    };
+
+});
