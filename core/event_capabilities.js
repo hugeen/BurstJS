b40857cf485e4f9b65b7caf64785128279e3b895
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2014-01-11 17:46:13 +0100

    Revert event capabilities

--- core/event_capabilities.js
+++ core/event_capabilities.js
@@ -8,69 +8,32 @@ define(function() {
             return object;
         }
 
-        var events = {
-            root: {}
+        var events = {};
+
+        object.on = function(identifier, fnc) {
+            events[identifier] = events[identifier] || [];
+            events[identifier].push(fnc);
+
+            return object;
         };
 
-        object.on = function(identifier, listener) {
-            if (Array.isArray(identifier)) {
-                var scopes = identifier;
-                identifier = identifier.pop();
-                scopes.forEach(function(scope) {
-                    pushListener(scope, identifier, listener);
-                });
-            } else {
-                pushListener("root", identifier, listener);
+        object.off = function(identifier, fnc) {
+            if (identifier in events === true) {
+                events[identifier].splice(events[identifier].indexOf(fnc), 1);
             }
+
+            return object;
         };
 
-        object.off = function(identifier, listener) {
-            if (Array.isArray(identifier)) {
-                var scopes = identifier;
-                identifier = identifier.pop();
-                scopes.forEach(function(scope) {
-                    removeListener(scope, identifier, listener);
-                });
-            } else {
-                removeListener("root", identifier, listener);
+        object.emit = function(identifier, fnc) {
+            if (identifier in events === true) {
+                for (var i = 0; i < events[identifier].length; i++) {
+                    events[identifier][i].apply(object, slice.call(arguments, 1));
+                }
             }
+
+            return object;
         };
-
-        object.emit = function(identifier) {
-            if (Array.isArray(identifier)) {
-                var scopes = identifier;
-                identifier = identifier.pop();
-                scopes.forEach(function(scope) {
-                    callListeners(scope, identifier, slice.call(arguments, 1));
-                });
-            } else {
-                callListeners("root", identifier, slice.call(arguments, 1));
-            }
-        };
-
-        function callListeners(scope, identifier, args) {
-            events[scope] = events[scope] || {};
-            if (identifier in events[scope] === false) {
-                return;
-            }
-            for (var i = 0; i < events[scope][identifier].length; i++) {
-                events[scope][identifier][i].apply(object, args);
-            }
-        }
-
-        function pushListener(scope, identifier, listener) {
-            events[scope] = events[scope] || {};
-            events[scope][identifier] = events[scope][identifier] || [];
-            events[scope][identifier].push(listener);
-        }
-
-        function removeListener(scope, identifier, listener) {
-            events[scope] = events[scope] || {};
-            if (identifier in events[scope] === false) {
-                return;
-            }
-            events[scope][identifier].splice(events[scope][identifier].indexOf(listener), 1);
-        }
 
         return object;
 
